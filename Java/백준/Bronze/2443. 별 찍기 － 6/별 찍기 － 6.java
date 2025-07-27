b0a54ed3ed3d2db07ab9097a48e6ed80c093c927
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2025-07-27 16:21:38 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 364 ms, Memory: 20384 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for(int i = n; i >= 1; i--)
+        {
+            for(int j = 0; j < n-i; j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < i; j++)
+            {
+                System.out.print("*");
+            }
+            for(int j = 0; j < i-1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
