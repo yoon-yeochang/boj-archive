25e183af62bc8725abc20683ce919567d050e678
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-02-26 20:20:05 +0900

    [Bronze V] Title: 윤년, Time: 100 ms, Memory: 14292 KB -BaekjoonHub

--- Java/백준/Bronze/2753. 윤년/윤년.java
+++ Java/백준/Bronze/2753. 윤년/윤년.java
@@ -1,18 +1,22 @@
+import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner scanner = new Scanner(System.in);
-        int year = scanner.nextInt();
-        
-        if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
-        {
-            System.out.println(1);
-        } else
-        {
-            System.out.println(0);
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int year = Integer.parseInt(st.nextToken());
+
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            sb.append(1);
+        } else {
+            sb.append(0);
         }
+
+        System.out.println(sb);
     }
 }
