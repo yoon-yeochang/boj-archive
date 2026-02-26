845ed1e20d4a1492c18bc2ba1b77803692756ae5
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-02-26 22:41:39 +0900

    [Bronze III] Title: 홀수, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

--- Java/백준/Bronze/2576. 홀수/홀수.java
+++ Java/백준/Bronze/2576. 홀수/홀수.java
@@ -1,29 +1,46 @@
+import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner scanner = new Scanner(System.in);
-        
-        int sum = 0;
-        List<Integer> odds = new ArrayList<>();
-        for(int i = 0; i < 7; i++)
-        {
-            int a = scanner.nextInt();
-            if(a % 2 == 1)
-            {
-                odds.add(a);
-                sum += a;
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int[] num1 = new int[7];
+        int[] num2 = new int[7];
+        int count = 0;
+
+        for (int i = 0; i < 7; i++) {
+            st = new StringTokenizer(br.readLine());
+            num1[i] = Integer.parseInt(st.nextToken());
+            if (num1[i] % 2 == 1) {
+                num2[count] = num1[i];
+                count++;
             }
         }
-        if(sum == 0)
-        {
-            System.out.println(-1);
-        } else
-        {
-            System.out.println(sum);
-            System.out.println(Collections.min(odds));
+        Arrays.sort(num2);
+
+        if (count > 0) {
+            int sum = 0;
+            for (int i = 0; i < 7; i++) {
+                sum += num2[i];
+            }
+
+            sb.append(sum).append("\n");
+
+            for (int i = 0; i < 7; i++) {
+                if (num2[i] != 0) {
+                    sb.append(num2[i]);
+                    break;
+                }
+            }
+        } else {
+            sb.append(-1);
         }
+
+
+        System.out.println(sb);
     }
 }
