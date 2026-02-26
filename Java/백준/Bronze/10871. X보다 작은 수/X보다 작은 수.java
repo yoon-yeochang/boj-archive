849006cd7f2ee620319ea4599905592942667cf0
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-02-26 16:16:02 +0900

    [Bronze V] Title: X보다 작은 수, Time: 136 ms, Memory: 15276 KB -BaekjoonHub

--- Java/백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ Java/백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,26 +1,28 @@
+import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int x = scanner.nextInt();
-        int[] arr = new int[n];
-        
-        for(int i = 0; i < n; i++)
-        {
-            arr[i] = scanner.nextInt();
-        }
-        
-        for(int i = 0; i < n; i++)
-        {
-            if(arr[i] < x)
-            {
-                System.out.print(arr[i] + " ");
-                
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+        int[] num = new int[10000];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+
+            if (num[i] < x) {
+                sb.append(num[i]).append(" ");
             }
         }
+
+        System.out.println(sb);
+
     }
 }
