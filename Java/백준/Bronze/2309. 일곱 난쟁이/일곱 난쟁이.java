95ae39e3f1a9bca73fbd810533d7ee30412c8e9b
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-02-26 23:12:57 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

--- Java/백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ Java/백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,35 +1,43 @@
+import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner scanner = new Scanner(System.in);
-        List<Integer> numbers = new ArrayList<>();
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int[] num = new int[9];
         int sum = 0;
-        for(int i = 0; i < 9; i++)
-        {
-            int n = scanner.nextInt();
-            numbers.add(n);
-            sum += n;
+        for (int i = 0; i < 9; i++) {
+            num[i] = Integer.parseInt(br.readLine());
+            sum += num[i];
         }
+
         int a = sum - 100;
-        for(int i = 0; i < 9; i++)
-        {
-            for(int j = i + 1; j < 9; j++)
-            {
-                if(numbers.get(i) + numbers.get(j) == a)
-                {
-                    numbers.remove(j);
-                    numbers.remove(i);
-                    Collections.sort(numbers);
-                    for(int n : numbers)
-                    {
-                        System.out.println(n);
-                    }
-                    return;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (num[i] + num[j] == a) {
+                    num[i] = 0;
+                    num[j] = 0;
+                    break;
                 }
             }
+            if (num[i] == 0) {
+                break;
+            }
         }
+
+        Arrays.sort(num);
+
+        for (int i = 0; i < 9; i++) {
+            if (num[i] != 0) {
+                sb.append(num[i]).append("\n");
+            }
+        }
+
+        System.out.println(sb);
     }
 }
