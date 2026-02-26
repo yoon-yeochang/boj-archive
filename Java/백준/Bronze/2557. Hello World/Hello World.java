b77656e22bd0c935fed192d6ed9dcfed04754abb
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-02-26 16:25:29 +0900

    [Bronze V] Title: Hello World, Time: 88 ms, Memory: 14048 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        sb.append("Hello World!");
+        
+        System.out.println(sb);
+    }
+}
