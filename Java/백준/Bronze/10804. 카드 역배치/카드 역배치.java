ac91f2e22f248e6c4b430f48050125f023c94ee4
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-02-27 21:31:03 +0900

    [Bronze II] Title: 카드 역배치, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

--- Java/백준/Bronze/10804. 카드 역배치/카드 역배치.java
+++ Java/백준/Bronze/10804. 카드 역배치/카드 역배치.java
@@ -1,37 +1,35 @@
+import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner scanner = new Scanner(System.in);
-        List<Integer> list = new ArrayList<>();
-        
-        for(int i = 1; i <= 20; i++)
-        {
-            list.add(i);
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+
+        for (int i = 0; i < 10; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            List<Integer> list = new ArrayList<>();
+            for (int j = a - 1; j <= b - 1; j++) {
+                list.add(arr[j]);
+            }
+
+            Collections.reverse(list);
+            for (int j = a - 1; j <= b - 1; j++) {
+                arr[j] = list.get(j - a + 1);
+            }
         }
-        
-        for(int i = 0; i < 10; i++)
-        {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            
-            List<Integer> part1 = new ArrayList<>(list.subList(0, (a-1)));
-            List<Integer> part2 = new ArrayList<>(list.subList((a-1), b));
-            List<Integer> part3 = new ArrayList<>(list.subList(b, 20));
-            
-            Collections.reverse(part2);
-            
-            list.clear();
-            list.addAll(part1);
-            list.addAll(part2);
-            list.addAll(part3);
+
+        for (int i : arr) {
+            sb.append(i).append(" ");
         }
-        
-        for(int n : list)
-        {
-            System.out.print(n + " ");
-        }
+
+        System.out.println(sb);
     }
 }
