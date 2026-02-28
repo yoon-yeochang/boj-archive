4f0f504dd4e53639fe38b92d5452fee2f2216acc
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-02-28 16:37:06 +0900

    [Silver V] Title: 방 번호, Time: 100 ms, Memory: 14376 KB -BaekjoonHub

--- Java/백준/Silver/1475. 방 번호/방 번호.java
+++ Java/백준/Silver/1475. 방 번호/방 번호.java
@@ -1,52 +1,34 @@
+import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        
-        String str = String.valueOf(n);
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[10];
-        
-        for(int i = 0; i < str.length(); i++)
-        {
-            arr[str.charAt(i) - '0'] += 1;
+
+        while (n > 0) {
+            arr[n % 10]++;
+            n /= 10;
         }
 
-        int max = 0;
-        
-        if(arr[6] >= arr[9])
-        {
-            max = arr[6] - ((arr[6] - arr[9]) / 2);
-            for(int i = 0; i < 10; i++)
-            {
-                if(i == 6 || i == 9)
-                {
-                    continue;
-                }
-                if(arr[i] > max)
-                {
-                    max = arr[i];
-                }
+        int max1 = 0;
+
+        for (int i = 0; i < 10; i++) {
+            if (i == 6 || i == 9) {
+                continue;
             }
-        } else
-        {
-            max = arr[9] - ((arr[9] - arr[6]) / 2);
-            for(int i = 0; i < 10; i++)
-            {
-                if(i == 6 || i == 9)
-                {
-                    continue;
-                }
-                if(arr[i] > max)
-                {
-                    max = arr[i];
-                }
+            if (max1 < arr[i]) {
+                max1 = arr[i];
             }
-        } 
-        
-        System.out.println(max);
+        }
+
+        int max2 = (arr[6] + arr[9]) / 2 + (arr[6] + arr[9]) % 2;
+
+        System.out.println(Math.max(max1, max2));
     }
 }
