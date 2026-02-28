02d8274adf0e59c52bd78e9615ad21dedb639e76
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-02-28 16:03:10 +0900

    [Bronze II] Title: 숫자의 개수, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- Java/백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ Java/백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,24 +1,29 @@
+import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner scanner = new Scanner(System.in);
-        long a = scanner.nextLong();
-        long b = scanner.nextLong();
-        long c = scanner.nextLong();
-        long n = a * b * c;
-        int[] arr = new int[10];
-        
-        String strn = String.valueOf(n);
-        for(int i = 0; i < strn.length(); i++)
-        {
-            arr[strn.charAt(i) - '0'] += 1;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        int num = a * b * c;
+
+        String str = Integer.toString(num);
+        int[] n = new int[10];
+
+        for (int i = 0; i < str.length(); i++) {
+            n[Integer.parseInt(String.valueOf(str.charAt(i)))]++;
         }
-        for(int i = 0; i < 10; i++)
-        {
-            System.out.println(arr[i]);
+
+        for (int i : n) {
+            sb.append(i).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
