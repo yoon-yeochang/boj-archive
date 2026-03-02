392b2e56a75f0417373e3672457e56739d34c874
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-03-02 12:16:15 +0900

    [Silver IV] Title: 제로, Time: 204 ms, Memory: 23828 KB -BaekjoonHub

--- Java/백준/Silver/10773. 제로/제로.java
+++ Java/백준/Silver/10773. 제로/제로.java
@@ -1,29 +1,32 @@
+import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner(System.in);
-        Deque<Integer> stack = new ArrayDeque<>();
-        int k = sc.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        for (int i = 0; i < k; i++)
-        {
-            int number = sc.nextInt();
-            if (number == 0)
-            {
+        int k = Integer.parseInt(br.readLine());
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < k; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            if (n == 0) {
                 stack.pop();
-            } else
-            {
-                stack.push(number);
+            } else {
+                stack.push(n);
             }
         }
+
         int sum = 0;
-        while(!stack.isEmpty())
-        {
-            sum += stack.pop();
+        for (Integer i : stack) {
+            sum += i;
         }
+
+
         System.out.println(sum);
     }
 }
