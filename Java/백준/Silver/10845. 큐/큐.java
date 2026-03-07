13dd94a7cd59ae3e9b0c22670b114bfe58448cd9
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-03-07 11:23:49 +0900

    [Silver IV] Title: 큐, Time: 160 ms, Memory: 19840 KB -BaekjoonHub

--- Java/백준/Silver/10845. 큐/큐.java
+++ Java/백준/Silver/10845. 큐/큐.java
@@ -1,72 +1,58 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main
-{
-    public static void main(String args[]) throws IOException
-    {
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
         Deque<Integer> queue = new ArrayDeque<>();
 
-        int n = Integer.parseInt(br.readLine().trim());
-
-        for(int i = 0; i < n; i++)
-        {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
             String str = st.nextToken();
 
-            if (str.equals("push"))
-            {
-                int x = Integer.parseInt(st.nextToken());
-                queue.offer(x);
-            } else if (str.equals("pop"))
-            {
-                Integer x = queue.poll();
-                if (x != null)
-                {
-                    sb.append(x).append("\n");
-                } else
-                {
-                    sb.append(-1).append("\n");
-                }
-            } else if (str.equals("size"))
-            {
-                int size = queue.size();
-                sb.append(size).append("\n");
-            } else if (str.equals("empty"))
-            {
-                boolean empty = queue.isEmpty();
-                if (empty)
-                {
-                    sb.append(1).append("\n");
-                } else
-                {
-                    sb.append(0).append("\n");
-                }
-            } else if (str.equals("front"))
-            {
-                Integer x = queue.peek();
-                if (x != null)
-                {
-                    sb.append(x).append("\n");
-                } else
-                {
-                    sb.append(-1).append("\n");
-                }
-            } else if (str.equals("back"))
-            {
-                Integer x = queue.peekLast();
-                if (x != null)
-                {
-                    sb.append(x).append("\n");
-                } else
-                {
-                    sb.append(-1).append("\n");
-                }
+            switch (str) {
+                case "push":
+                    int x = Integer.parseInt(st.nextToken());
+                    queue.offer(x);
+                    break;
+                case "pop":
+                    if (!queue.isEmpty()) {
+                        sb.append(queue.poll()).append("\n");
+                    } else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+                case "empty":
+                    if (queue.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "front":
+                    if (!queue.isEmpty()) {
+                        sb.append(queue.peekFirst()).append("\n");
+                    } else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+                case "back":
+                    if (!queue.isEmpty()) {
+                        sb.append(queue.peekLast()).append("\n");
+                    } else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
             }
         }
-
         System.out.println(sb);
     }
 }
