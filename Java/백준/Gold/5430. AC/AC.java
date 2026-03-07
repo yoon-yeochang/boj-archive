8e6e6e03dfc96be81a4f1a816e736dc9b57003b8
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-03-07 15:10:16 +0900

    [Gold V] Title: AC, Time: 736 ms, Memory: 94424 KB -BaekjoonHub

--- Java/백준/Gold/5430. AC/AC.java
+++ Java/백준/Gold/5430. AC/AC.java
@@ -14,64 +14,55 @@ public class Main {
 
             int n = Integer.parseInt(br.readLine());
 
-            String input = br.readLine();
-            String cleaned = input.replace("[", "").replace("]","");
-            String[] x = cleaned.split(",");
-
-            int count = 0;
-            for (char c : parr) {
-                if (c == 'D') {
-                    count++;
-                }
-            }
-
-            if (count > n) {
-                sb.append("error").append("\n");
-                continue;
-            } else if (n == 0) {
-                sb.append("[]").append("\n");
-                continue;
-            }
+            st = new StringTokenizer(br.readLine(), "[,]");
 
             Deque<Integer> deque = new ArrayDeque<>();
-            for (String s : x) {
-                deque.offerLast(Integer.parseInt(s));
+            while (st.hasMoreTokens()) {
+                deque.offerLast(Integer.parseInt(st.nextToken()));
             }
 
             boolean isReversed = false;
-            for (char c : parr) {
-                switch (c) {
-                    case 'R':
-                        isReversed = !isReversed;
+            boolean isError = false;
+
+            for (char cmd : parr) {
+                if (cmd == 'R') {
+                    isReversed = !isReversed;
+                } else {
+                    if (deque.isEmpty()) {
+                        isError = true;
                         break;
-                    case 'D':
-                        if (isReversed) {
-                            deque.pollLast();
-                        } else {
-                            deque.pollFirst();
-                        }
-                        break;
+                    }
+                    if (isReversed) {
+                        deque.pollLast();
+                    } else {
+                        deque.pollFirst();
+                    }
                 }
             }
 
-            sb.append("[");
-            int a = deque.size();
-            if (isReversed) {
-                for (int j = 0; j < a; j++) {
-                    sb.append(deque.pollLast());
-                    if (j != a - 1) {
-                        sb.append(",");
-                    }
-                }
+            if (isError) {
+                sb.append("error").append("\n");
             } else {
-                for (int j = 0; j < a; j++) {
-                    sb.append(deque.pollFirst());
-                    if (j != a - 1) {
-                        sb.append(",");
+                sb.append("[");
+                int a = deque.size();
+                if (isReversed) {
+                    for (int j = 0; j < a; j++) {
+                        sb.append(deque.pollLast());
+                        if (j != a - 1) {
+                            sb.append(",");
+                        }
+                    }
+                } else {
+                    for (int j = 0; j < a; j++) {
+                        sb.append(deque.pollFirst());
+                        if (j != a - 1) {
+                            sb.append(",");
+                        }
                     }
                 }
+                sb.append("]").append("\n");
             }
-            sb.append("]").append("\n");
+
         }
 
         System.out.println(sb);
