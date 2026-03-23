7d637836086f745bb78c0f59da025fb2b62a89bf
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-03-23 16:37:43 +0900

    [Silver III] Title: N과 M (3), Time: 372 ms, Memory: 67856 KB -BaekjoonHub

---
+++ Java/백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static int m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        func(1, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void func(int cur, int count) {
+        if (count == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = cur; i <= n; i++) {
+            arr[count] = i;
+            func(cur, count + 1);
+        }
+    }
+}
