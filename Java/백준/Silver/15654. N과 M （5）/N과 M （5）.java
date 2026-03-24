b065aead21bb2f60e8a62b47bdfc492ccc4826ca
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-03-24 17:26:10 +0900

    [Silver III] Title: N과 M (5), Time: 276 ms, Memory: 28480 KB -BaekjoonHub

---
+++ Java/백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static int m;
+    static int[] number;
+    static int[] arr;
+    static boolean[] isUsed = new boolean[10001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        number = new int[n];
+        arr = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(number);
+
+        func(0);
+
+        System.out.println(sb);
+    }
+
+    private static void func(int k) {
+        if (k == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (!isUsed[number[i]]) {
+                isUsed[number[i]] = true; 
+                arr[k] = number[i];
+                func(k + 1);
+                isUsed[number[i]] = false;
+            }
+        }
+    }
+}
