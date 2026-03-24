4da2052c01cfae67320f7d0e15eb8d3058855f7a
Yoon-YeoChang <162788829+yoon-yeochang@users.noreply.github.com>
2026-03-24 22:48:27 +0900

    [Silver II] Title: N과 M (10), Time: 108 ms, Memory: 14228 KB -BaekjoonHub

---
+++ Java/백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,57 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static int m;
+    static int[] number;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        number = new int[n];
+        arr = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(number);
+
+        func(0, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void func(int k, int cur) {
+        if (k == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int temp = 0;
+
+        for (int i = cur; i < n; i++) {
+            if (number[i] == temp) {
+                continue;
+            }
+
+            arr[k] = number[i];
+            func(k+1, i + 1);
+            temp = number[i];
+        }
+    }
+}
